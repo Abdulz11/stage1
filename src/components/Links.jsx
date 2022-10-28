0b853fdaa9e8d9936a89b7bbef03153a55db2114
Abdulz11 <abduloyewale@gmail.com>
2022-10-28 22:40:35 +0100

    link names given

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,12 +1,40 @@
 import React from 'react'
 
 export default function () {
+    const slackUserName = "JustAbdul"
   return (
     <div className='link-container'>
-        <button className='btn'>Link</button>
-        <button className='btn'>Link</button>
-        <button className='btn'>Link</button>
-        <button className='btn'>Link</button>
+        <button className='btn'>
+            <a  id='btn__zuri' href="https://training.zuri.team/">
+               <p>ZURI</p>
+            </a>
+        </button>
+        <button className='btn'>
+            <a id='books' href="https://books.zuri.team">
+               <p>GET BOOKS</p>
+               <p>Thousands of books to help you master design and coding</p>
+            </a>
+        </button>
+        <button className='btn'>
+            <a id='book__python' href={`https://books.zuri.team/python-for-beginners?ref_id=${slackUserName}`}>
+              <p> WANT TO LEARN PYTHON</p>
+              <p>Get the python-for-beginners to help start your journey as a  python programmer for only $4.99  </p>
+
+            </a>
+        </button>
+        <button className='btn'>
+            <a  id='pitch' href="https://background.zuri.team">
+                <p>BACKGROUND CHECKS</p>
+                <p>  Find out who your team members really are with  background check for coders</p>
+            </a>
+        </button>
+        <button className='btn'>
+            <a id='book__design' href="https://books.zuri.team/design-rules">
+              <p>READY TO UP YOUR DESIGN SKILLS</p>
+             <p> Get the ultimate design rules  book from zuri for FREE</p>
+            </a>
+        </button>
+       
     </div>
   )
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-// import profileimg from "./image/istockphoto-1190297071-612x612.jpg"
+import profile from "../image/photo-1628890923662-2cb23c2e0cfe.avif"
 
 export default function () {
   return (
    <section className='profile'>
     <div className='profile-container'>
-        <img src='' alt="profile photo" />
-        <a href="#">Connect on twitter</a>
-        {/* slackUsername = JustAbdul */}
-        <hr />
+      <div className='img-div'>
+        <img id="profile__img" src={profile} alt="profile photo" />
+      </div>
+      <a  id="twitter" href="https://www.twitter.com/@abdul_oyewale">@abdul_oyewale</a>
+      {/* <a  id="slack" href="https//:www.slack.com/JustAbdul">Connect on slack</a> */}
     </div>
    </section>
   )
